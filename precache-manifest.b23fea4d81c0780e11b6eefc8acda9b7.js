11a929f2c9e5effc066a3ea048d98c68690d33cd
Thiago de Sa <thiagodesa26@gmail.com>
2020-04-29 08:35:16 -0300

    Updates

---
+++ precache-manifest.b23fea4d81c0780e11b6eefc8acda9b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4627340c6d6fa439dd72ac240ce24ad5",
+    "url": "/monsters-react/index.html"
+  },
+  {
+    "revision": "8d3e2bc30894b90884f7",
+    "url": "/monsters-react/static/css/main.901bc4df.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-react/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-react/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d3e2bc30894b90884f7",
+    "url": "/monsters-react/static/js/main.be493305.chunk.js"
+  },
+  {
+    "revision": "bb10d4d4a0632a4e8caf",
+    "url": "/monsters-react/static/js/runtime-main.2624874f.js"
+  }
+]);
